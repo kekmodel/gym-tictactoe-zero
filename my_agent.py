bdffb024f316d833650d7ab8c29b771fa8f354ed
kekmodel <kekmodel@gmail.com>
2018-01-07 21:31:24 +0900

    dirichlet noise 추가

--- my_agent.py
+++ my_agent.py
@@ -2,6 +2,7 @@
 from tictactoe_env import TicTacToeEnv
 from gym.utils import seeding
 import numpy as np
+import time
 import h5py
 import math
 from collections import deque, defaultdict
@@ -11,7 +12,7 @@ PLAYER = 0
 OPPONENT = 1
 MARK_O = 2
 N, W, Q, P = 0, 1, 2, 3
-episode_count = 400
+episode_count = 100
 
 
 # 에이전트 클래스 (실제 플레이 용, 예시)
@@ -35,12 +36,15 @@ class ZeroAgent(object):
 # edge[좌표행][좌표열][요소번호]로 요소 접근
 class MCTS(object):
     def __init__(self):
+        # memories
         self.state_memory = deque(maxlen=9 * episode_count)
         self.node_memory = deque(maxlen=9 * episode_count)
         self.edge_memory = deque(maxlen=9 * episode_count)
-        self.pi_memory = deque(maxlen=9 * episode_count)
+        self.piq_memory = deque(maxlen=9 * episode_count)
+
+        # reset steap member
         self.tree_memory = None
-        self.action_memory = None
+        self.pr = None
         self.puct = None
         self.edge = None
         self.pi = None
@@ -48,15 +52,20 @@ class MCTS(object):
         self.empty_loc = None
         self.total_visit = None
         self.first_turn = None
+
+        # reset episode member
+        self.action_memory = None
         self.action_count = None
         self.board = None
         self.state = None
+
         self._reset_step()
         self._reset_episode()
 
         # 하이퍼파라미터
         self.c_puct = 5
         self.epsilon = 0.25
+        self.alpha = 1.5
         self.seed()
 
     def seed(self, seed=None):
@@ -70,6 +79,7 @@ class MCTS(object):
         self.total_visit = 0
         self.legal_move_n = 0
         self.empty_loc = None
+        self.pr = 0
         self.tree_memory = defaultdict(lambda: 0)
 
     def _reset_episode(self):
@@ -80,16 +90,23 @@ class MCTS(object):
 
     def select_action(self, state):
         self.action_count += 1
-        self.state = state
+        # save raw state
+        self.state_memory.appendleft(state)
+        self.state = np.copy(state)
+        # state를 hash로 변환 (dict의 key로 쓰려고)
+        state_hash = hash(self.state.tostring())
+        # hash 저장
+        self.node_memory.appendleft(state_hash)
         # 호출될 때마다 첫턴 기준 교대로 행동주체 바꿈, 최종 action에 붙여줌
         user_type = (self.first_turn + self.action_count) % 2
         self.init_edge()
         self._cal_puct()
+        print(self.puct, file=f)
         puct = self.puct.tolist()
         for i, v in enumerate(puct):
             for k, s in enumerate(v):
                 if [i, k] not in self.empty_loc.tolist():
-                    puct[i][k] = -99999
+                    puct[i][k] = -9999
         self.puct = np.asarray(puct)
         tmp = np.argwhere(self.puct == self.puct.max()).tolist()
         for i, v in enumerate(tmp):
@@ -117,14 +134,23 @@ class MCTS(object):
             self.board = self.state[PLAYER] + self.state[OPPONENT]
             self.empty_loc = np.asarray(np.where(self.board == 0)).transpose()
             self.legal_move_n = self.empty_loc.shape[0]
-            pr = round(1 / self.legal_move_n, 5)
+            prob = round(1 / self.legal_move_n, 5)
+            # root node 면
+            if self.action_count == 0:
+                self.pr = (1 - self.epsilon) * prob + self.epsilon * \
+                    self.np_random.dirichlet(
+                        self.alpha * np.ones(self.legal_move_n))
+            else:
+                self.pr = prob * np.ones(self.legal_move_n)
             for i in range(self.legal_move_n):
                 self.edge[self.empty_loc[i][0]
-                          ][self.empty_loc[i][1]][P] = pr
+                          ][self.empty_loc[i][1]][P] = self.pr[i]
         else:
+            self.pr = pr
             for i in range(3):
                 for k in range(3):
-                    self.edge[i][k][P] = pr[i][k]
+                    self.edge[i][k][P] = self.pr[i][k]
+        # edge 저장
         self.edge_memory.appendleft(self.edge)
 
     def _cal_puct(self):
@@ -169,14 +195,8 @@ class MCTS(object):
                                 ][self.action_memory[i][2]][N] += 1
         self._reset_episode()
 
-    def _cal_pi(self, tau=0):
-        if tau == 0:
-            for i in range(len(self.edge_memory)):
-                for r in range(3):
-                    for c in range(3):
-                        self.pi[r][c] = self.edge_memory[i][r][c][N]
-                self.pi_memory.appendleft(self._softmax(self.pi.flatten()))
-            return self.pi_memory
+    def _cal_pi(self, edge):
+        return
 
     def _softmax(self, visit_count):
         e_x = np.exp(visit_count - np.max(visit_count))
@@ -185,6 +205,8 @@ class MCTS(object):
 
 
 if __name__ == "__main__":
+    start = time.time()
+    f = open("data/result.txt", "w")
     # 환경 생성 및 시드 설정
     env = TicTacToeEnv()
     env.seed(2018)
@@ -195,45 +217,67 @@ if __name__ == "__main__":
     result = {1: 0, 0: 0, -1: 0}
     play_mark_O = 0
     win_mark_O = 0
-    # train data 생성
+    # train data 생성용
     for e in range(episode_count):
         state = env.reset()
-        print('-' * 15, '\nepisode: %d' % (e + 1))
+        print('-' * 22, '\nepisode: %d' % (e + 1))
+        f = open("data/result.txt", "a")
+        print('-' * 22, '\nepisode: %d' % (e + 1), file=f)
         # 첫턴을 나와 상대 중 누가 할지 정하기
         selfplay.first_turn = selfplay.np_random.choice(2, replace=False)
+        # 첫턴인 경우 기록
+        if selfplay.first_turn == PLAYER:
+            play_mark_O += 1
         done = False
         while not done:
-            # state를 hash로 변환 (dict의 key로 쓰려고)
-            state_copy = state.copy()
-            state_hash = hash(state_copy.tostring())
-            # hash 저장
-            selfplay.node_memory.appendleft(state_hash)
-            # raw state 저장
-            selfplay.state_memory.appendleft(state)
-            # print(state[PLAYER] + state[OPPONENT] * 2)
+            # state 텍스트 파일에 기록
+            print(state[PLAYER] + state[OPPONENT] * 2, file=f)
             # action 선택하기
             action = selfplay.select_action(state)
             # action 진행
             state, reward, done, info = env.step(action)
         if done:
             # 승부난 후 보드 보기: 플레이어 착수:1 상대착수:2
-            print(state[PLAYER] + state[OPPONENT] * 2)
+            print(state[PLAYER] + state[OPPONENT] * 2, file=f)
             # 보상을 edge에 백업
             selfplay.backup(reward, info)
             # 결과 dict에 기록
             result[reward] += 1
-            # print(len(selfplay.node_memory))
-            if env.mark_O == PLAYER:
-                play_mark_O += 1
-                if reward == 1:
+            # 결과를 텍스트 파일에 기록
+            if reward == 1:
+                print('You Win!', info, file=f)
+                if env.mark_O == PLAYER:
                     win_mark_O += 1
-    # 에피소드 통계 내기
-    print('-' * 15, '\nWin: %d Lose: %d Draw: %d Winrate: %0.1f%% PlayMarkO: %d WinMarkO: %d' %
+            elif reward == -1:
+                print('You Lose!', info, file=f)
+            else:
+                print('Draw!', info, file=f)
+        # train data 저장
+        if e % 1 == 0:
+            with h5py.File('data/state_memory.hdf5', 'w') as h5s:
+                h5s.create_dataset("state", data=selfplay.state_memory)
+            with h5py.File('data/edge_memory.hdf5', 'w') as h5e:
+                h5e.create_dataset("edge", data=selfplay.edge_memory)
+
+    # 통계 출력
+    # 화면
+    print('-' * 22, '\nWin: %d Lose: %d Draw: %d Winrate: %0.1f%% PlayMarkO: %d WinMarkO: %d' %
           (result[1], result[-1], result[0], result[1] / episode_count * 100, play_mark_O, win_mark_O))
-    # 데이터 저장
-    with h5py.File('state_memory.hdf5', 'w') as hf:
-        hf.create_dataset("zero_data_set", data=selfplay.state_memory)
-    with h5py.File('edge_memory.hdf5', 'w') as hf:
-        hf.create_dataset("zero_data_set", data=selfplay.edge_memory)
-    # 신경망 학습
-    # 신경망 평가
+    # 텍스트 파일
+    print('-' * 22, '\nWin: %d Lose: %d Draw: %d Winrate: %0.1f%% PlayMarkO: %d WinMarkO: %d' %
+          (result[1], result[-1], result[0], result[1] / episode_count * 100, play_mark_O, win_mark_O), file=f)
+    f.close()
+    finish = round(float(time.time() - start), 1)
+    # data 불러오기
+    '''
+    hfs = h5py.File('data/state_memory.hdf5', 'r+')
+    state_memory = hfs.get('state')
+    state_memory = deque(state_memory)
+    hfs.close()
+    hfe = h5py.File('data/edge_memory.hdf5', 'r+')
+    edge_memory = hfe.get('edge')
+    edge_memory = deque(edge_memory)
+    hfe.close()
+    print('state: {}'.format(state_memory))
+    # print('edge: {}'.format(edge_memory))
+    '''
