9118b11953a1d197bd45c3ff71f389f15e590061
kekmodel <kekmodel@gmail.com>
2018-02-01 04:28:30 +0900

    Update!

--- neural_network_cpu.py
+++ neural_network_cpu.py
@@ -15,7 +15,7 @@ class PolicyValueNet(nn.Module):
         self.conv_bn = nn.BatchNorm2d(32)
         self.conv_relu = nn.ReLU(inplace=True)
 
-        # residual layer
+        # residual layer 1
         self.conv1 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
         self.conv1_bn = nn.BatchNorm2d(32)
         self.conv1_relu = nn.ReLU(inplace=True)
@@ -23,6 +23,42 @@ class PolicyValueNet(nn.Module):
         self.conv2_bn = nn.BatchNorm2d(32)
         # forward엔 여기에 skip connection 추가 필요
         self.conv2_relu = nn.ReLU(inplace=True)
+
+        # residual layer 2
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv3_bn = nn.BatchNorm2d(32)
+        self.conv3_relu = nn.ReLU(inplace=True)
+        self.conv4 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv4_bn = nn.BatchNorm2d(32)
+        # forward엔 여기에 skip connection 추가 필요
+        self.conv4_relu = nn.ReLU(inplace=True)
+
+        # residual layer 3
+        self.conv5 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv5_bn = nn.BatchNorm2d(32)
+        self.conv5_relu = nn.ReLU(inplace=True)
+        self.conv6 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv6_bn = nn.BatchNorm2d(32)
+        # forward엔 여기에 skip connection 추가 필요
+        self.conv6_relu = nn.ReLU(inplace=True)
+
+        # residual layer 4
+        self.conv7 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv7_bn = nn.BatchNorm2d(32)
+        self.conv7_relu = nn.ReLU(inplace=True)
+        self.conv8 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv8_bn = nn.BatchNorm2d(32)
+        # forward엔 여기에 skip connection 추가 필요
+        self.conv8_relu = nn.ReLU(inplace=True)
+
+        # residual layer 5
+        self.conv9 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv9_bn = nn.BatchNorm2d(32)
+        self.conv9_relu = nn.ReLU(inplace=True)
+        self.conv10 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
+        self.conv10_bn = nn.BatchNorm2d(32)
+        # forward엔 여기에 skip connection 추가 필요
+        self.conv10_relu = nn.ReLU(inplace=True)
 
         # 정책 헤드: 정책함수 인풋 받는 곳
         self.policy_head = nn.Conv2d(32, 2, kernel_size=1)
@@ -55,10 +91,12 @@ class PolicyValueNet(nn.Module):
                 m.bias.data.zero_()
 
     def forward(self, state):
-
+        # convolutional layer
         x = self.conv(state)
         x = self.conv_bn(x)
         x = self.conv_relu(x)
+
+        # residual layer 1
         residual = x
         x = self.conv1(x)
         x = self.conv1_bn(x)
@@ -68,6 +106,47 @@ class PolicyValueNet(nn.Module):
         x += residual  # skip connection
         x = self.conv2_relu(x)
 
+        # residual layer 2
+        residual = x
+        x = self.conv3(x)
+        x = self.conv3_bn(x)
+        x = self.conv3_relu(x)
+        x = self.conv4(x)
+        x = self.conv4_bn(x)
+        x += residual  # skip connection
+        x = self.conv4_relu(x)
+
+        # residual layer 3
+        residual = x
+        x = self.conv5(x)
+        x = self.conv5_bn(x)
+        x = self.conv5_relu(x)
+        x = self.conv6(x)
+        x = self.conv6_bn(x)
+        x += residual  # skip connection
+        x = self.conv6_relu(x)
+
+        # residual layer 4
+        residual = x
+        x = self.conv7(x)
+        x = self.conv7_bn(x)
+        x = self.conv7_relu(x)
+        x = self.conv8(x)
+        x = self.conv8_bn(x)
+        x += residual  # skip connection
+        x = self.conv8_relu(x)
+
+        # residual layer 5
+        residual = x
+        x = self.conv9(x)
+        x = self.conv9_bn(x)
+        x = self.conv9_relu(x)
+        x = self.conv10(x)
+        x = self.conv10_bn(x)
+        x += residual  # skip connection
+        x = self.conv10_relu(x)
+
+        # policy head
         p = self.policy_head(x)
         p = self.policy_bn(p)
         p = self.policy_relu(p)
@@ -75,6 +154,7 @@ class PolicyValueNet(nn.Module):
         p = self.policy_fc(p)
         p = self.policy_softmax(p)
 
+        # value head
         v = self.value_head(x)
         v = self.value_bn(v)
         v = self.value_relu1(v)
