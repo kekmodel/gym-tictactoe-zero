a5fa157be6d0c4c22486b9b9a5da8c8bccfbcc34
kekmodel <kekmodel@gmail.com>
2018-02-11 16:41:07 +0900

    Simplify

--- tictactoe_env.py
+++ tictactoe_env.py
@@ -1,32 +1,42 @@
 # -*- coding: utf-8 -*-
 import gym   # 환경 제공 모듈
+
 import numpy as np   # 배열 제공 모듈
+"""
+소개
+==================================================================
 
+규칙: O, X 를 번갈아 가면서 표시하고 3개 연속으로 한줄을 채우면 승리, 무승부 있음.
 
-''' 소개 -----------------------------------------------------------
-# 규칙: O, X 를 번갈아 가면서 표시하고 3개 연속으로 한줄을 채우면 승리, 무승부 있음
-# state: (3, 3, 3) 넘파이 배열: 3*3 평면 3장
- 0번 평면: 나의 표시만 1로 체크
- 1번 평면: 상대 표시만 1로 체크 (현재 셀프 플레이만 지원)
- 2번 평면: "O"만 1로 체크 (누가 "OX"인지 구별 용)
-# action: tuple(피아식별, 좌표행, 좌표열)
- ex) action = (0, 1, 1) -> step(action) -> state[0][1][1] = 1
-* 0번 평면을 기준으로 승패체크. 보상 (승:1, 무:0, 패:-1)
-* "O"표시 인식하여 2번 평면에 동기화 함
-[
-[[0, 0, 0]
- [0, 0, 0]
- [0, 0, 0]]  0번 평면
+    0번 평면을 기준으로 승패체크. 보상 (승:1, 무:0, 패:-1)
+    "O"표시인 주체를 인식하여 2번 평면에 동기화 함
+    self-play만 지원
+------------------------------------------------------------------
 
-[[0, 0, 0]
- [0, 0, 0]
- [0, 0, 0]]  1번 평면
+state: 3x3x3 numpy 배열: 3x3 평면 3장.
+    0번 평면: 나의 표시만 1로 체크
+    1번 평면: 상대 표시만 1로 체크
+    2번 평면: "O"만 1로 체크 (누가 "OX"인지 구별 용)
+        [
+        [[0, 0, 0]
+         [0, 0, 0]
+         [0, 0, 0]]  0번 평면
 
-[[0, 0, 0]
- [0, 0, 0]
- [0, 0, 0]]  2번 평면
- ]
---------------------------------------------------------------- '''
+        [[0, 0, 0]
+         [0, 0, 0]
+         [0, 0, 0]]  1번 평면
+
+        [[0, 0, 0]
+         [0, 0, 0]
+         [0, 0, 0]]  2번 평면
+        ]
+-------------------------------------------------------------------
+
+action: tuple(피아식별, 좌표행, 좌표열).
+    action = (0, 1, 1) -> step(action) -> state[0][1][1] = 1
+
+====================================================================
+"""
 
 PLAYER = 0  # 플레이어 식별 상수
 OPPONENT = 1  # 상대 식별 상수
@@ -36,39 +46,48 @@ MARK_X = 1
 
 
 class TicTacToeEnv(gym.Env):
-    """gym.Env를 상속하여 틱택토 게임 환경 클래스 정의
-        reset()후 plyer_color를 반드시 설정해야 함
-        gym.Env: OpenAI Gym의 주요 클래스, 환경 뒤에서 이루어지는 동작 캡슐화
-        (gym/core.py 참조)
+    """틱택토 게임 환경 제공 클래스.
+
+    plyer_color 를 반드시 설정해야 함: reset()하면 재설정 필요함.
+        >> env = TicTacToeEnv()
+        >> state = env.reset()
+        >> env.player_color = MARK_O
+
+    gym.Env: gym/core.py 참조.
     """
     # _render()의 리턴 타입 구분
     metadata = {'render.modes': ['human', 'rgb_array']}
     reward = (-1, 0, 1)  # 보상의 범위 참고: 패배:-1, 무승부:0, 승리:1
 
     def __init__(self):
-        self.state = None  # 상태
+        self.state = None
         self.viewer = None  # 뷰어
         self.player_color = None  # 나의 "OX"
+        self._reset()
 
-    def _reset(self):  # 상태 리셋 함수
-        # 상태 초기화 (3*3 개짜리배열 3장) 2진으로만 해결하기 위해!
+    def _reset(self):
+        """state 리셋 함수.
+
+        state 초기화: 3x3 배열 3장: 2진으로만 해결하기 위함.
+        """
         self.state = np.zeros((3, 3, 3), 'int')
         self.viewer = None   # 뷰어 리셋
-        self.step_count = 0  # 액션 진행 횟수
+        self.step_count = 0  # action 진행 횟수
         self.player_color = None
-        return self.state  # 상태 리턴
+        return self.state  # state 리턴
 
     def _step(self, action):
-        """한번의 행동에 상태가 어떻게 변하는지 정하는 함수
-            승부가 나면 에이전트가 reset()을 호출하여 환경을 초기화 해야 함
-            action을 받아서 (state, reward, done, info)인 튜플 리턴 함
+        """한번의 action에 state가 어떻게 변하는지 정하는 메소드.
+
+        승부가 나면 에이전트가 reset()을 호출하여 환경을 초기화 해야 함.
+        action을 받아서 (state, reward, done, info)인 튜플 리턴 함.
         """
-        # 규칙 위반 필터링: 액션 자리에 이미 자리가 차있으면
+        # 규칙 위반 필터링: 착수 금지: action 자리에 이미 자리가 차있으면
         if self.state[action] == 1:
             if action[USER_TYPE] == PLAYER:  # 근데 그게 플레이어가 한 짓이면 반칙패
                 reward = -1
                 done = True  # 게임 종료
-                info = {'steps': self.step_count}  # 액션 1회로 인정
+                info = {'steps': self.step_count}  # action 1회로 인정
                 print('Illegal Lose!')  # 출력
                 return self.state, reward, done, info  # 필수 요소 리턴
             elif action[USER_TYPE] == OPPONENT:  # 상대가 한 짓이면 반대
@@ -78,13 +97,14 @@ class TicTacToeEnv(gym.Env):
                 print('Illegal Win!')
                 return self.state, reward, done, info
 
+        # action 적용
         self.state[action] = 1
 
-        # 중복 착수, color 미제출 시 오류 발생
+        # 연속 두번 하기, player_color 비설정 시 오류 발생시키기
         redupl = np.sum(self.state[PLAYER]) - np.sum(self.state[OPPONENT])
         if abs(redupl) > 1 or self.player_color is None:
             raise NotImplementedError
-        # "O"가 아닌데 처음에 하면 오류 발생
+        # "O"가 아닌데 처음에 하면 오류 발생시키기
         if self.player_color != MARK_O:
             if np.sum(self.state) == 1 and action[USER_TYPE] == PLAYER:
                 raise NotImplementedError
@@ -92,14 +112,16 @@ class TicTacToeEnv(gym.Env):
             if np.sum(self.state) == 1 and action[USER_TYPE] == OPPONENT:
                 raise NotImplementedError
 
-        # 2번 보드 동기화
+        # 2번 보드("O") 동기화
         if self.player_color == MARK_O:
             self.state[2] = self.state[PLAYER]
         else:
             self.state[2] = self.state[OPPONENT]
+
         return self.__check_win()  # 승패 체크해서 리턴
 
-    def __check_win(self):  # state 승패체크용 내부 함수
+    def __check_win(self):
+        """state 승패체크용 내부 함수."""
         # 승리패턴 8가지 구성 (1:돌이 있는 곳, 0: 돌이 없는 곳)
         win_pattern = np.array([[[1, 1, 1], [0, 0, 0], [0, 0, 0]],
                                 [[0, 0, 0], [1, 1, 1], [0, 0, 0]],
@@ -109,9 +131,10 @@ class TicTacToeEnv(gym.Env):
                                 [[0, 0, 1], [0, 0, 1], [0, 0, 1]],
                                 [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
                                 [[0, 0, 1], [0, 1, 0], [1, 0, 0]]])
+        # 0,1번 보드가 승리패턴과 일치하면
         for i in range(2):
-            for k in range(8):  # 0,1번 보드가 승리패턴과 일치하면
-                # 바이너리 배열은 패턴을 포함할때 서로 곱(행렬곱아님)하면 패턴 자신이 나옴
+            for k in range(8):
+                # 2진 배열은 패턴을 포함할때 서로 곱(행렬곱 아님)하면 패턴 자신이 나옴
                 if np.array_equal(
                         self.state[i] * win_pattern[k], win_pattern[k]):
                     if i == PLAYER:  # i가 플레이어면 승리
@@ -126,21 +149,23 @@ class TicTacToeEnv(gym.Env):
                         info = {'steps': self.step_count}  # step 수 기록
                         print('You Lose!', info)  # 너 짐
                         return self.state, reward, done, info  # 필수 요소 리턴!
-        # 다 돌려봤는데 승부난게 없더라 근데 O식별용 2번보드에 들어있는게 5개면? 비김
+        # 다 돌려봤는데 승부난게 없더라 근데 "O"식별용 2번보드에 들어있는게 5개면? 비김
         if np.count_nonzero(self.state[2]) == 5:
             reward = 0  # 보상 0
             done = True  # 게임 끝
             info = {'steps': self.step_count}
             print('Draw!', info)  # 비김
             return self.state, reward, done, info
-        else:  # 이거 다~~~ 아니면 다음 수 둬야지
+        # 이거 다~~~ 아니면 다음 수 둬야지
+        else:
             reward = 0
             done = False  # 안 끝남!
             info = {'steps': self.step_count}
             return self.state, reward, done, info
 
-    def _render(self, mode='human', close=False):  # 현재 상태를 그려주는 함수
-        if close:   # 클로즈값이 참인데
+    def _render(self, mode='human', close=False):
+        """현재 state를 그려주는 함수."""
+        if close:  # 클로즈값이 참인데
             if self.viewer is not None:  # 뷰어가 비어있지 않으면
                 self.viewer.close()   # 뷰어를 닫고
                 self.viewer = None   # 뷰어 지우기
@@ -251,8 +276,9 @@ class TicTacToeEnv(gym.Env):
             trans_X9 = rendering.Transform(render_loc[8])
             self.image_X9.add_attr(trans_X9)
 
-        # ------------ 상태 정보에 맞는 이미지를 뷰어에 붙이는 과정 ------------- #
-        # 좌표번호마다 O,X가 있는지 확인하여 해당하는 이미지를 뷰어에 붙임 (렌더링 때 보임)
+        # ------------ state 정보에 맞는 이미지를 뷰어에 붙이는 과정 ------------- #
+        # 좌표번호마다 "OX"가 있는지 확인하여 해당하는 이미지를 뷰어에 붙임 (렌더링 때 보임)
+        # "OX"의 정체성 설정!
         render_O = None
         render_X = None
         if self.player_color == MARK_O:
@@ -306,5 +332,6 @@ class TicTacToeEnv(gym.Env):
             self.viewer.add_geom(self.image_O9)
         elif self.state[render_X][2][2] == 1:
             self.viewer.add_geom(self.image_X9)
-        # rgb 모드면 뷰어를 렌더링해서 리턴해라
+
+        # rgb 모드면 뷰어를 렌더링해서 리턴
         return self.viewer.render(return_rgb_array=mode == 'rgb_array')
