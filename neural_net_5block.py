6c2206bdb04f5d6acfc94283f8ee99978af109d4
kekmodel <kekmodel@gmail.com>
2018-03-01 07:23:45 +0900

    Test

--- neural_net_5block.py
+++ neural_net_5block.py
@@ -76,7 +76,7 @@ class PolicyValueNet(nn.Module):
         self.value_scalar = nn.Linear(channel, 1)
         self.value_out = nn.Tanh()
 
-        """# weight 초기화 (xavier)
+        # weight 초기화 (xavier)
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -89,7 +89,6 @@ class PolicyValueNet(nn.Module):
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
-        """
 
     def forward(self, state):
         # convolutional layer
