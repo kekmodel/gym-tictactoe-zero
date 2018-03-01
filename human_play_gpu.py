2dee03a272fe515da871c921428209d0f4f94550
kekmodel <kekmodel@gmail.com>
2018-03-01 20:05:57 +0900

    Add data files

--- human_play_gpu.py
+++ human_play_gpu.py
@@ -35,7 +35,7 @@ class MCTS:
         # model
         self.pv_net = neural_net_5block.PolicyValueNet(CHANNEL).cuda()
         if model_path is not None:
-            print('#######  Model is loaded  #######')
+            print(' #######  Model is loaded  #######')
             self.pv_net.load_state_dict(torch.load(model_path))
 
         self.done = False
